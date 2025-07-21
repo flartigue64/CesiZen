42a69f3e4f167fb6e429769e6c8a03572839fab8
fabien.lartigue <f.lartigue@citosav.com>
2025-07-21 14:05:31 +0200

    delete pomalo

--- CESIZen/Data/DesignTimeDbContextFactory.cs
+++ CESIZen/Data/DesignTimeDbContextFactory.cs
@@ -1,17 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using CesiZen.Data;
+using CESIZen.Data;
 
 public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CesiZenDbContext>
 {
     public CesiZenDbContext CreateDbContext(string[] args)
     {
-        var connectionString = "Server=127.0.0.1;Port=3306;Database=CESIZenDb;User=root;Password=MonMotDePasse123!;";
-
         var optionsBuilder = new DbContextOptionsBuilder<CesiZenDbContext>();
-        
-        // Use a specific MySQL version instead of ServerVersion.AutoDetect to avoid connection issues
-        optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 21)));
+
+        // Remplace la chaîne de connexion par celle de SQL Server
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CESIZenDb;Trusted_Connection=True;");
 
         return new CesiZenDbContext(optionsBuilder.Options);
     }
