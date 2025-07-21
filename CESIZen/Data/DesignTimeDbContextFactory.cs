6a51b092aad406a95b86acfc62c54f57af7a2295
fabien.lartigue <f.lartigue@citosav.com>
2025-07-21 14:09:08 +0200

    change namespace designdb

--- CESIZen/Data/DesignTimeDbContextFactory.cs
+++ CESIZen/Data/DesignTimeDbContextFactory.cs
@@ -1,16 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using CESIZen.Data;
 
-public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CesiZenDbContext>
+namespace CesiZen.Data
 {
-    public CesiZenDbContext CreateDbContext(string[] args)
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CesiZenDbContext>
     {
-        var optionsBuilder = new DbContextOptionsBuilder<CesiZenDbContext>();
+        public CesiZenDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CesiZenDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CESIZenDb;Trusted_Connection=True;");
 
-        // Remplace la chaîne de connexion par celle de SQL Server
-        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CESIZenDb;Trusted_Connection=True;");
-
-        return new CesiZenDbContext(optionsBuilder.Options);
+            return new CesiZenDbContext(optionsBuilder.Options);
+        }
     }
 }
